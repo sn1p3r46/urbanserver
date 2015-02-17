f3cf24881aa7120ce2e9e5fa62f08a0dbeb06217
sn1p3r46 <kingokongo46@gmail.com>
2015-02-17 02:27:22 +0100

    Funonzia?

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,5 +2,5 @@ import sys, os
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), '/var/www'))
 os.environ['DJANGO_SETTINGS_MODULE'] = "UrbanServerApp_project.settings"
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
