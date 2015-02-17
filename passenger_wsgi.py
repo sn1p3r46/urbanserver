469f49c12c1abeb79c5bbb816be050f40a0ecdff
sn1p3r46 <kingokongo46@gmail.com>
2015-02-17 02:21:00 +0100

    Funziona?

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), '/var/www'))
-os.environ['DJANGO_SETTINGS_MODULE'] = "cpc.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "UrbanServerApp_project.settings"
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
