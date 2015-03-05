2fd7028722339656fb2f09739d17c5a589db4638
sn1p3r46 <kingokongo46@gmail.com>
2015-03-05 18:14:33 +0100

    fdefsda

--- UrbanServerApp_project/settings.py
+++ UrbanServerApp_project/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media/')
 
 
+
 print __file__
 print os.path.dirname(__file__)
 print os.path.dirname(os.path.dirname(__file__))
